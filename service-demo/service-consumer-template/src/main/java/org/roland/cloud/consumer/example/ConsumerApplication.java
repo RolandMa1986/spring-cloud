5ca30c493d0fd72c34fb8f0a391b4e25d81f4b46
Roland Ma <rolandma@outlook.com>
2020-05-16 17:15:07 +0800

    use undertow

--- service-demo/service-consumer-template/src/main/java/org/roland/cloud/consumer/example/ConsumerApplication.java
+++ service-demo/service-consumer-template/src/main/java/org/roland/cloud/consumer/example/ConsumerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
-@EnableDiscoveryClient(autoRegister = true)
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableOAuth2Client
 public class ConsumerApplication {
